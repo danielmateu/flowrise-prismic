76b3acca14f37e494c6822535d0bbca8f19431ba
Daniel Mateu <danielmateu86@gmail.com>
2023-11-30 13:54:45 +0100

    metadata added

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <h1 className="font-display text-5xl">Hello world</h1>
-      <p className="font-body">Nunito sans, Hello world</p>
     </main>
   )
 }
