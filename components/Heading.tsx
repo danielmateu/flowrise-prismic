3b42eaf2b12cdd1e8b97be6c581c8270a5b3c0e3
Daniel Mateu <danielmateu86@gmail.com>
2023-12-01 10:21:36 +0100

    refactoring components

---
+++ components/Heading.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx"
+
+interface HeadingProps {
+    as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+    size?: 'sm' | 'md' | 'lg' | 'xl'
+    children: React.ReactNode
+    className?: string
+}
+
+export const Heading = ({
+    as: Comp = 'h1',
+    className,
+    children,
+    size = 'lg'
+}: HeadingProps) => {
+    return (
+        <Comp className={clsx("text-5xl font-bold leading-tight tracking-tight  font-display text-slate-700 text-center",
+            size === 'xl' && 'text-5xl md:text-7xl',
+            size === 'lg' && 'text-4xl md:text-5xl',
+            size === 'md' && 'text-3xl md:text-4xl',
+            size === 'sm' && 'text-2xl md:text-3xl',
+            className
+        )}>
+            {children}
+        </Comp>
+    )
+}
--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,9 +1,18 @@
 import Bounded from "@/components/Bounded";
 import Button from "@/components/Button";
+import { Heading } from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
+const components: JSXMapSerializer = {
+  heading1: ({ children }) => (
+    <Heading as='h1' size="xl" className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb-0">{children}</Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="font-body text-2xl text-slate-600 leading-10 mb-4 md:mb-8 max-w-xl text-center">{children}</p>
+  )
+}
 /**
  * Props for `Hero`.
  */
@@ -18,16 +27,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <PrismicRichText field={slice.primary.heading} components={{
-        heading1: ({ children }) => (
-          <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight  font-display text-slate-700 text-center">{children}</h1>
-        )
-      }} />
-      <PrismicRichText field={slice.primary.body} components={{
-        paragraph: ({ children }) => (
-          <p className="font-body text-2xl text-slate-600 leading-10 mb-4 md:mb-8 max-w-xl text-center">{children}</p>
-        )
-      }} />
+      <PrismicRichText field={slice.primary.heading} components={components} />
+      <PrismicRichText field={slice.primary.body} components={components} />
       <Button field={slice.primary.button_link}
         className="mb-20">
         {slice.primary.button_text}
@@ -36,7 +37,6 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl" />
 
       {/* Placeholder component for hero (variation: {slice.variation}) Slices!!! */}
-
     </Bounded>
   );
 };
