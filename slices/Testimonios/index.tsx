7cf12bcc0974a21b21e82b8ca668b79c2c780f93
Daniel Mateu <danielmateu86@gmail.com>
2023-12-04 17:09:11 +0100

    testimonios added

--- slices/Testimonios/index.tsx
+++ slices/Testimonios/index.tsx
@@ -3,10 +3,12 @@ import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismic
 import Bounded from "@/components/Bounded";
 import { Heading } from "@/components/Heading";
 import { createClient } from "@/prismicio";
+import Image from "next/image";
+import { PrismicNextImage } from "@prismicio/next";
 
 const components: JSXMapSerializer = {
   heading2: ({ children }) => (
-    <Heading as='h2' size="md" className="">
+    <Heading as='h2' size="md" className="text-center mb-10">
       {children}
     </Heading>
   ),
@@ -41,10 +43,18 @@ const Testimonios = async ({ slice }: TestimoniosProps): Promise<JSX.Element> =>
       data-slice-variation={slice.variation}
     >
       <PrismicRichText field={slice.primary.heading} components={components} />
-      <div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
         {testimonios.map((item, index) => item && (
-          <div key='index'>
+          <div key={index} className="border bg-white shadow-lg hover:shadow-sm transition rounded-lg px-8 md:px-14 py-10 md:py-16 flex flex-col gap-6">
             <PrismicRichText field={item.data.quote} components={components} />
+            {/* Imagen */}
+            <div className="flex gap-2 items-center">
+              <PrismicNextImage field={item.data.avatar} width={56} height={56} className="rounded-full" imgixParams={{ ar: "1:1", fit: "crop" }} />
+              <div>
+                <p className="font-semibold">{item.data.name} </p>
+                <p className="text-slate-500 text-sm">{item.data.job_title}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
