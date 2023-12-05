6c1fee5a84d11868cb27f2f1dc46a67deb07e0cc
Daniel Mateu <danielmateu86@gmail.com>
2023-12-05 12:17:21 +0100

    Prismic Preview added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import './globals.css'
 import clsx from 'clsx'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { createClient } from '@/prismicio'
+import { createClient, repositoryName } from '@/prismicio'
+import { PrismicPreview } from '@prismicio/next'
+
 
 // const inter = Inter({ subsets: ['latin'] })
 const nunito = Nunito({
@@ -44,7 +46,8 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
-        <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-100 z-[-1] inset-0 opacity-50"/>
+        <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-100 z-[-1] inset-0 opacity-50" />
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   )
